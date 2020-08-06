ef12439617b5a991ca04c33a62ed2fc162dc4c65
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-08-07 01:35:56 +0300

    finnished connecting the data

--- app/src/main/java/kapadokia/nyandoro/moviemvvm/data/repository/MovieDataSource.kt
+++ app/src/main/java/kapadokia/nyandoro/moviemvvm/data/repository/MovieDataSource.kt
@@ -1,21 +1,67 @@
 package kapadokia.nyandoro.moviemvvm.data.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kapadokia.nyandoro.moviemvvm.data.api.FIRST_PAGE
+import kapadokia.nyandoro.moviemvvm.data.api.TheMovieDbInterface
 import kapadokia.nyandoro.moviemvvm.data.vo.Movie
 
-class MovieDataSource : PageKeyedDataSource<Int, Movie>() {
-    override fun loadInitial(
-        params: LoadInitialParams<Int>,
-        callback: LoadInitialCallback<Int, Movie>
-    ) {
+class MovieDataSource (private val apiService:TheMovieDbInterface, private val compositeDisposable: CompositeDisposable)
+    : PageKeyedDataSource<Int, Movie>() {
 
+    private val TAG = "MovieDataSource"
+
+    private var page = FIRST_PAGE;
+    val networkState: MutableLiveData<NetworkState> = MutableLiveData()
+
+    override fun loadInitial(params: LoadInitialParams<Int>,callback: LoadInitialCallback<Int, Movie>) {
+            networkState.postValue(NetworkState.LOADING)
+
+        compositeDisposable.add(
+            apiService.getPopularMovie(page) // this will return a single observable
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        // successful movie fetched
+                        callback.onResult(it.movieList, null, page+1)
+                        networkState.postValue(NetworkState.LOADED)
+                    },
+                    {
+                        networkState.postValue(NetworkState.ERROR)
+                        Log.e(TAG, "loadInitial: "+ it.message)
+                    })
+        )
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
 
+        networkState.postValue(NetworkState.LOADING)
+
+        compositeDisposable.add(
+            apiService.getPopularMovie(params.key) // this will return a single observable
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        if (it.totalPages>=params.key){
+
+                            callback.onResult(it.movieList,  params.key+1)
+                            networkState.postValue(NetworkState.LOADED)
+                        }else{
+                            networkState.postValue(NetworkState.ENDOFLIST)
+                        }
+                    },
+                    {
+                        networkState.postValue(NetworkState.ERROR)
+                        Log.e(TAG, "loadInitial: "+ it.message)
+                    })
+        )
+
     }
 
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
-        
+
     }
 }
